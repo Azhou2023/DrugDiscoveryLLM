7a19c95255e2b5055b7b050d98f859988fcd9806
Azhou2023 <anz008@ucsd.edu>
2025-02-22 17:44:36 -0800

    testing framework

--- inference_loop.py
+++ inference_loop.py
@@ -12,22 +12,23 @@ from api import get_docking_data
 
 load_dotenv()
 
-def inference(protein, num_iterations, information="basic"):
-    system_instructions = "You are an assistant helping a lab generate ligands that can bind to certain proteins with high binding affinity."
+def inference(protein, num_iterations, information):
+    system_instructions = f"We will collaborate on generating a ligand for the {protein} protein with high binding affinity. I will give you the output from docking software after each of your attempts. Assume that there are always more improvements to be made to the current ligand."
     client = genai.Client(api_key=os.environ["API_KEY"])
     chat = client.chats.create(model="gemini-2.0-flash", config=types.GenerateContentConfig(system_instruction=system_instructions))
 
-    init_prompt = f"We will collaborate on generating a ligand for the {protein} protein with high binding affinity. I will give you the output from docking software after each attempt. Make sure your response contains a SMILES string at the end, and make sure that the SMILES string is valid. Start by generating an initial ligand."
+    init_prompt = f"Generate a ligand that can bind to {protein} with high binding affinity. Isolate your final answer in SMILES encoding format at the very end. Do not wrap the answer as a code block, and do not provide any text after the SMILES string."
 
     try:
         response = chat.send_message(init_prompt)
+        open(f'logs.txt', 'w').write(response.text+"\n")
         ligand = response.text.split()[-1]
         data = get_docking_data(ligand, protein)
         invalid_ligand = False
         if data:
             print(f"Initial generation: Molecule: {ligand} | Binding affinity: {data["binding_affinity"] if not invalid_ligand else 0}")
         else:
-            print("Initial generation: Invalid ligand")
+            print(f"Initial generation: Invalid ligand {ligand}")
             invalid_ligand = True
     except Exception as e:
         print(str(e))
@@ -45,7 +46,7 @@ def inference(protein, num_iterations, information="basic"):
             if information == "Basic":
                 iterated_prompt = f"Software shows that the molecule you generated ({ligand}) had a binding affinity of {data["binding_affinity"]} to {protein}. Based on this information, generate a better ligand, following the same answer format"
             elif information == "Extra":
-                iterated_prompt = [f"Software shows that the molecule you generated, {ligand}, had a binding affinity of {data["binding_affinity"]} to {protein} and formed {data["hydrogen_bonds"]} hydrogen bonds. Pictures of the interaction between the ligand and protein are also included, and the protein surface is colored by electrostatics. First, describe what you see in the image, and relate it to the observed binding affinity. Then discuss how to improve the binding. Based on this information, generate a better ligand, following the same answer format. Please keep trying improvements, and never say the ligand is final. Ensure the last word of your response is a valid SMILES string.",
+                iterated_prompt = [f"Software shows that the molecule you generated ({ligand}) had a binding affinity of {data["binding_affinity"]} to {protein} and formed {data["hydrogen_bonds"]} hydrogen bonds. Pictures of the interaction between the ligand and protein are also included, and the protein surface is colored by electrostatics. First, describe what you see in the image, and relate it to the observed binding affinity. Then discuss how to improve the binding. Based on this information, generate a better ligand, following the same answer format (do not provide any text after the SMILES string). Assume that there are always more improvements to be made to the current ligand, and always try to iterate further.",
                                    types.Part.from_bytes(data=data["images"]["view0"], mime_type="image/jpeg"), types.Part.from_bytes(data=data["images"]["view1"], mime_type="image/jpeg"), types.Part.from_bytes(data=data["images"]["view2"], mime_type="image/jpeg")]
             else:
                 rand = random.uniform(-15, -5)
@@ -54,7 +55,7 @@ def inference(protein, num_iterations, information="basic"):
             iterated_prompt = f"Software shows that the molecule you generated ({ligand}) had a binding affinity of 0 for {protein}. Generate a new ligand, following the same answer format."
         try:
             response = chat.send_message(iterated_prompt)
-            open(f'imgs/{i+1}.txt', 'w').write(response.text)
+            open(f'logs.txt', 'w').write(response.text+"\n")
             ligand = response.text.split()[-1]
 
             data = get_docking_data(ligand, protein)
@@ -68,7 +69,7 @@ def inference(protein, num_iterations, information="basic"):
                 print(f"Iteration {i+1}: Molecule: {ligand} | Binding affinity: {data["binding_affinity"]}")
             else:
                 invalid_ligand=True
-                print(f"Iteration {i+1}: Invalid ligand")
+                print(f"Iteration {i+1}: Invalid ligand {ligand}")
         except Exception as e:
             print(str(e))
             time.sleep(60)
